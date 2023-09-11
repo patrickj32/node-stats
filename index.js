b054b784c1741ce3dc758f75f8f8382658d3e2c5
Patrick Justice <80479370+patrickj32@users.noreply.github.com>
2023-09-11 19:11:05 -0400

    first commit

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ let ss = require("simple-statistics");
 //create an array of numnbers to work with
 let numbers = [62, 60, 53, 9, 11, 14, 15, 16, 17, 65, 88];
 
-//use the min() method in the simple-statitics package to get the smallest numnber in the array
 let smallestNumber = ss.min(numbers);
 let largestNumber = ss.max(numbers)
 let sumOfNum = ss.sumSimple(numbers)
 let meanOfNum = ss.mean(numbers)
 let roundedMean = Math.round(meanOfNum);
 
-//output the smallest number to the console
+
 console.log(smallestNumber)
 console.log(largestNumber)
 console.log(sumOfNum)
